6593632314481219df0ac3c3f6d7b3028e183003
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-03-26 11:56:18 +0200

    recreated base for backend

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,12 +1,5 @@
 import express from "express";
-import usersRoutes from "../routes/routes.js";
 const app = express();
-
-app.use("/", usersRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
