d45b5d1a7bc17b781f0a4d65df641a25ef252970
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-03-26 12:25:57 +0200

    [WIP] userentity+controller+db connection for auth

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import userRouter from "./routes/user_routes";
+import database from "./database/database";
 const app = express();
+
+app.use("/api/users", userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
