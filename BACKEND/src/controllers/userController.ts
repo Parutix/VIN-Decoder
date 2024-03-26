93bde5c90cc778892d152925495c179eef4af145
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-03-26 17:38:45 +0200

    [WIP] implemented user creation in db

--- BACKEND/src/controllers/userController.ts
+++ BACKEND/src/controllers/userController.ts
@@ -1,4 +1,14 @@
 import { Request, Response } from "express";
-import db from "../database/database";
+import create_user from "../services/userService";
 
-export const insert_user = (req: Request, res: Response) => {};
+const register_user = async (req: Request, res: Response) => {
+  try {
+    const user = req.body;
+    await create_user(req, res);
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status(400).send(`Error creating user: ${error}`);
+  }
+};
+
+export default register_user;
